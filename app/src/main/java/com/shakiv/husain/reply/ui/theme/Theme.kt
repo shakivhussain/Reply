cc585b649cc14942035997d2021aa46bdf6c5a9b
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-05-16 14:32:26 +0530

    ✅ Adding dynamic colors in app

--- app/src/main/java/com/shakiv/husain/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/shakiv/husain/reply/ui/theme/Theme.kt
@@ -1,10 +1,19 @@
 package com.shakiv.husain.reply.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 // Material 3 color schemes
 private val DarkColorScheme = darkColorScheme(
@@ -93,12 +102,32 @@ fun ReplyTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
 
-    val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
+
+    val colors =
+        when{
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ->{
+                if (useDarkTheme) dynamicDarkColorScheme(context)
+                else dynamicLightColorScheme(context)
+            }
+            useDarkTheme -> DarkColors
+            else -> LightColors
+        }
+
+
+    // Add primary status bar color from chosen color scheme.
+    val view = LocalView.current
+    if (!view.isInEditMode){
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.primary.toArgb()
+
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = useDarkTheme
+        }
     }
+
 
     MaterialTheme(
         colorScheme = colors,
